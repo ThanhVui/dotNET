a86f3c0f44870c86a8c0f29dffad026da4a5ce44
ThanhVui <thanhvuikg100@gmail.com>
2025-04-04 20:16:02 +0700

    Upload Session 1

---
+++ HelloWorld02/HelloWorld02/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
