614a1bc27b6d2a4917acdde1c251d3cb66174eb9
ThanhVui <thanhvuikg100@gmail.com>
2025-04-08 21:05:27 +0700

    Upload Mylibrary

---
+++ MyLibrary/Poem/Program.cs
@@ -0,0 +1,19 @@
+﻿using Poem.SongCode;
+
+namespace Poem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+            ThanhVui.ThanhVuiFunction();
+        }
+
+        public static void ThanhVuiFunction()
+        {
+            Console.WriteLine("Hello Thanh Vui, Learn C#!");
+        }
+    }
+}
---
+++ MyLibrary/Poem/SongCode/ThanhVui.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poem.SongCode
+{
+    internal class ThanhVui
+    {
+        public static void ThanhVuiFunction()
+        {
+            Console.WriteLine("Hello Thanh Vui, Learn C#!");
+        }
+    }
+}
